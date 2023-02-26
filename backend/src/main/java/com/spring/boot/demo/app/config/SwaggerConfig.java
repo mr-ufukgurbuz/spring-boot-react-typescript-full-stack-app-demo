35912457bed422d62c8b37f1dc45ce89489a1b18
ufukgurbuz <mr.ufukgurbuz@gmail.com>
2023-02-26 14:39:58 +0300

    #9 Added Swagger documention support

---
+++ backend/src/main/java/com/spring/boot/demo/app/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.spring.boot.demo.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+@EnableSwagger2WebMvc
+public class SwaggerConfig
+{
+
+    public static final Contact DEFAULT_CONTACT = new Contact("Ufuk Gurbuz", "http://www.deneme-spring-boot.com.tr", "mr.ufukgurbuz@gmail.com");
+
+    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+            "Swagger API Title", "Swagger API Description", "1.0",
+            "urn:tos", DEFAULT_CONTACT,
+            "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", Arrays.asList());
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(Arrays.asList("application/json", "application/xml"));
+
+    @Bean
+    public Docket api()
+    {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO).produces(DEFAULT_PRODUCES_AND_CONSUMES).consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+    }
+}
